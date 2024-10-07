0db21a86a8f6cdf9bb989e4cf99d63ced2051a96
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-07 12:25:43 -0600

    Tell the app to listen for requests on port 3000

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-// TODO: this file :)
+// Import express and create the express app
 const express = require("express");
 const app = express();
 
 const PORT = 3000;
+
+// Middleware goes here
+
+// Tell the app to start listening for requests
+app.listen(PORT, () => {
+  console.log(`Listening on port #${PORT}`);
+});
